552f323e30b2215a1f0bec9e5f4c3d4ade5dcac7
Chris Williams <chris.williams@mixrad.io>
2016-03-16 16:33:40 +0000

    Limit values to 0-9

--- src/mainController.js
+++ src/mainController.js
@@ -2,4 +2,8 @@ app.controller('mainController', function($scope) {
     $scope.gridValues=[[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],
     [0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],
     [0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5]];
+    $scope.increaseVal = function(x,y) {
+        if ($scope.gridValues[x][y] > 8) $scope.gridValues[x][y] = 0;
+        else $scope.gridValues[x][y]++;
+    }
 });
