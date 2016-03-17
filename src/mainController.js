87552535f7d77ed5b010d59cb006d7cc7c6696d7
Chris Williams <chris.williams@mixrad.io>
2016-03-17 13:31:30 +0000

    Data initialisation

--- src/mainController.js
+++ src/mainController.js
@@ -1,9 +1,27 @@
 app.controller('mainController', function($scope) {
-    $scope.gridValues=[[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],
-    [0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],
-    [0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5],[0,1,0,2,0,0,0,0,5]];
+   $scope.gridValues = [9];
+   for (i = 0; i < 10; i++)
+   {
+        $scope.gridValues[i] = [9];
+        for (j=0; j < 10; j++)
+        {
+            $scope.gridValues[i][j] = 0;
+        }
+    }
     $scope.increaseVal = function(x,y) {
         if ($scope.gridValues[x][y] > 8) $scope.gridValues[x][y] = 0;
         else $scope.gridValues[x][y]++;
+    }
+    $scope.solve = function() {
+        $scope.log1 = "Solving...";
+        for (i = 0; i < 10; i++)
+        {
+            $scope.log2 = i;
+             for (j=0; j < 10; j++)
+             {
+                 $scope.log3 = j;
+             }
+         }
+         $scope.log1 = "Solved";
     }
 });
